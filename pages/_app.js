afc8d82530662d708e0a6205dbfe10adc4f7b5b7
kidus-m <kidus0237@gmail.com>
2025-09-25 15:09:59 +0300

    minor change

--- pages/_app.js
+++ pages/_app.js
@@ -59,7 +59,7 @@ export default function App({ Component, pageProps }) {
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -20 }}
                         transition={{ duration: 0.5, ease: "easeInOut" }}
-                        className="min-h-screen"
+                        className=""
                     >
                         <Component {...pageProps} />
                     </motion.div>
