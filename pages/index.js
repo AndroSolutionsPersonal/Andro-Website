8cc1b5b033a48b0516fd70d119224d22f6ad80e5
kidus-m <kidus0237@gmail.com>
2025-09-25 10:22:12 +0300

    minor fixes on the index layout by removing the nav bar and footer

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,17 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import ContactSection from "@/components/ContactSection";
 import ServicesSection from "@/components/Services";
 import MissionVision from "@/components/MissionVision";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
-  return (
-    <main className="font-sans">
-      <section className='flex flex-col w-screen items-center justify-center'>
-        <Navbar />
-      </section>
-      <Hero />
-      <ServicesSection />
-        <MissionVision />
-      <section className="px-2 pb-2 md:px-4 md:pb-4 overflow-hidden">
-        <ContactSection />
-      </section>
-      <footer className="px-2 pb-2 md:px-4 md:pb-4">
-        <Footer />
-      </footer>
-    </main>
-  );
+    return (
+        <>
+            <Hero />
+            <ServicesSection />
+            <MissionVision />
+            <section className="px-2 pb-2 md:px-4 md:pb-4 overflow-hidden">
+                <ContactSection />
+            </section>
+        </>
+    );
 }
