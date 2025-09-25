4b6d622137247d3d05c8474cab792c9ba3e842a3
kidus-m <kidus0237@gmail.com>
2025-09-25 10:23:20 +0300

    added the navbar and footer the app page to make it for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <main className="font-sans">
+        {/* Navbar always on top */}
+        <section className="flex flex-col w-screen items-center justify-center">
+          <Navbar />
+        </section>
+
+        {/* Page content */}
+        <Component {...pageProps} />
+
+        {/* Footer always at bottom */}
+        <footer className="px-2 pb-2 md:px-4 md:pb-4">
+          <Footer />
+        </footer>
+      </main>
+  );
 }
